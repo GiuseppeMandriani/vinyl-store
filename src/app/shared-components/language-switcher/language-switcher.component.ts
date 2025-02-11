3b0765bb4a6775446b19e71f539da0fa56f5c69f
Giuseppe Mandriani <giuseppemandriani@MBP-di-Serafina.Home>
2025-02-11 09:23:14 +0100

    feat: created component language-switcher wip

---
+++ src/app/shared-components/language-switcher/language-switcher.component.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-language-switcher',
+  standalone: true,
+  imports: [CommonModule, TranslateModule],
+  templateUrl: './language-switcher.component.html',
+  styleUrl: './language-switcher.component.scss'
+})
+export class LanguageSwitcherComponent {
+
+  @Input() languages: string[] = ['en', 'it']; // Lingue disponibili
+  @Input() currentLang: string = 'it'; // Lingua predefinita
+
+  @Output() changeLang = new EventEmitter<string>();
+
+  constructor(private translateService: TranslateService) {
+    this.translateService.setDefaultLang(this.currentLang);
+    this.translateService.use(this.currentLang);
+  }
+
+
+  public onLanguageChange(event: Event): void {
+    const selectedLanguage = (event.target as HTMLSelectElement).value;
+    this.changeLang.emit(selectedLanguage); // Emissione della lingua selezionata
+  }
+
+}
